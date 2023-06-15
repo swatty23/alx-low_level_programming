edf05e3946e95b63649a31095db9a651e178d17f
swatty23 <swattymovine@gmail.com>
2023-06-15 04:20:06 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - printf "Programming is like building a multilingual puzzle, followed by a new line.
+ * main - printf "Programming is like building a multilingual 
+ * puzzle, followed by a new line.
  * Return: Always 0.
  */
 
