6604ad9087a83ea0097978fb6adf427628049784
swatty23 <swattymovine@gmail.com>
2023-06-20 04:14:35 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,16 @@
 
 /**
  * main - check the code
- *
- * Return: Always 0.
+ * Return: void
  */
-int main(void)
+
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
