69565427d95287c08d77b78860ff88841946f137
swatty23 <swattymovine@gmail.com>
2023-06-20 08:38:25 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,13 +1,29 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdio.h>
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+	if (j < 4000000 && (j % 2) == 0)
+	{
+	sum = sum + j;
+	}
+	next = j + k;
+	j = k;
+	k = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
 }
